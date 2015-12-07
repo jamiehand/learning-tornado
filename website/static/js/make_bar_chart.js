dbadc7a850001714d813456edfeea485e038392d
jamiehand <hand.jamie@gmail.com>
2015-12-07 12:41:24 -0500

    Get labels, word cloud working

--- website/static/js/make_bar_chart.js
+++ website/static/js/make_bar_chart.js
@@ -14,8 +14,10 @@ var app = app || {}; // Could have 3 files that add things to this one object.
     var dataURL = "/es/";
 
     /* width, height, location of svg */
-    var w = 200;
-    var h = 230;
+    var w = {};
+    var h = {};
+    w.default = 200;
+    h.default = 230;
     var locationInDOM = "body";
 
     /* bar formatting */
@@ -35,31 +37,28 @@ var app = app || {}; // Could have 3 files that add things to this one object.
      * d object is passed into it. */
     // TODO is this being used? Should I change it to involve tagNames
     // or remove it?
-    var key = function(d) {
-        return d.key;
-    };
+    // var key = function(d) {
+    //     return d.key;
+    // };
 
     /* don't run automatically; allow user to load in their own parameters */
     var buildBarChart = function(params){
         dataURL = params.dataURL || dataURL;
-        w = params.width || w;
-        h = params.height || h;
+        w.bars = params.width || w.default;
+        h.bars = params.height || h.default;
         locationInDOM = params.locationInDOM || locationInDOM;
         barPadding = params.barPadding || barPadding;
         barColor = params.barColor || barColor;
         barLabelColor = params.barLabelColor || barLabelColor;
         tagNameColor = params.tagNameColor || tagNameColor;
         clickHandler = params.clickHandler || clickHandler;
-        key = params.keyFunction || key;
+        // key = params.keyFunction || key;
 
         /* 'function' is a callback function to be run once /es/ has
          * been fetched. */
         d3.json(dataURL, function(
             error, jsonList) {
             if (error) return console.warn(error);
-            // console.log(jsonList[0]._source.authors)
-            // console.log(jsonList);
-            // console.log(dataset);
 
             dataset = getTagCounts(jsonList); // pass the jsonList dict to separate its tags
             console.log(dataset);
@@ -98,9 +97,23 @@ var app = app || {}; // Could have 3 files that add things to this one object.
      * {tag: "foo", count: 2}) */
 
     function drawBarsAndLabels(dataset, locationInDOM, clickHandler) {
-        /* TODO label on left with tagName */
+
+        var max_tagName_len_pixels;
+        var left_margin;
+        var char_width = 6; // assume char width is 6 pixels
+                            // TODO better soln for this?
 
         var tagNames = Object.getOwnPropertyNames(dataset);
+
+        /* don't let tagNames be more than 1/3 of svg width */
+        max_tagName_len_pixels = Math.min(
+            (d3.max(tagNames, function(tag){ return tag.length; }) * char_width),
+            w.bars/3
+        );
+        max_tagName_len_chars = max_tagName_len_pixels / char_width;
+
+        left_margin = max_tagName_len_pixels + 5;
+        console.log(max_tagName_len_pixels);
 
         /* scales
          * Note: min/max/extent takes in an array, and a fcn saying how
@@ -114,7 +127,10 @@ var app = app || {}; // Could have 3 files that add things to this one object.
                                 return dataset[d];
                               })])
                              .range(
-                              [barPadding, w - barPadding * 2]);
+                              [left_margin, w.bars - barPadding * 2]);
+
+        console.log("right side: " + (w.bars - barPadding * 2));
+
 
         /* sort tagNames according to tag count */
         tagNames.sort(function(tag1, tag2){
@@ -127,7 +143,7 @@ var app = app || {}; // Could have 3 files that add things to this one object.
 
 
         /* make list smaller if it's too many tags */
-        var maxNumTags = Math.floor(h/minBarPlusPaddingHeight);
+        var maxNumTags = Math.floor(h.bars/minBarPlusPaddingHeight);
         if(tagNames.length > maxNumTags){
             isEveryTagDisplayedInBarChart = false;
             var tempTagNames = [];
@@ -138,8 +154,8 @@ var app = app || {}; // Could have 3 files that add things to this one object.
         }
 
         var svg = d3.select(locationInDOM).append("svg");
-        svg.attr("width", w)
-           .attr("height", h);
+        svg.attr("width", w.bars)
+           .attr("height", h.bars);
         var rects = svg.selectAll("rect")
                        .data(tagNames) // pass in the list of properties
                        .enter()
@@ -148,17 +164,17 @@ var app = app || {}; // Could have 3 files that add things to this one object.
                            return xScale(0);
                        })
                        .attr("y", function(d, i) {
-                           return i * (h / tagNames.length);
+                           return i * (h.bars / tagNames.length);
                        })
                        .attr("width", function(d){
-                           return xScale(dataset[d]);
+                           return xScale(dataset[d]) - left_margin; // TODO best way to manage left margin?
                        })
-                       .attr("height", h / tagNames.length - barPadding)
+                       .attr("height", h.bars / tagNames.length - barPadding)
                        .attr("fill", barColor);
 
         rects.on("click", clickHandler);
 
-        svg.selectAll("text")
+        svg.selectAll("text.values") // these are 'text' tags with class 'values'
            .data(tagNames)
            .enter()
            .append("text")
@@ -166,21 +182,173 @@ var app = app || {}; // Could have 3 files that add things to this one object.
                return dataset[d];
            })
            .attr("x", function(d) {
-               return xScale(dataset[d]) - 5;
+               return xScale(dataset[d]) - char_width;
            })
            .attr("y", function(d, i){
-               return ((i * (h / tagNames.length)
-                    + (h / tagNames.length - barPadding + 7) / 2));
+               return ((i * (h.bars / tagNames.length)
+                    + (h.bars / tagNames.length - barPadding + 7) / 2));
            })
-           .attr("font-family", "sans-serif")  // TODO why does changing these not
+           .attr("font-family", "sans-serif")  // TODO why does changing color/size not
            .attr("font-size", "11px")          // seem to affect the visualization?
            .attr("fill", barLabelColor)
-           .attr("text-anchor", "middle");
+           .attr("text-anchor", "middle")
+           .attr("class", "values");
+
+        svg.selectAll("text.labels")
+           .data(tagNames)
+           .enter()
+           .append("text")
+           .text(function(d){
+               /* modified from http://stackoverflow.com/a/7463674/4979097 */
+               var trimmedString = d.length > max_tagName_len_chars ?
+                   d.substring(0, max_tagName_len_chars - 3) + "..." :
+                   d.substring(0, max_tagName_len_chars);
+               return trimmedString;
+           })
+           .attr("x", 10) // indent 10
+           .attr("y", function(d, i){
+               return ((i * (h.bars / tagNames.length)
+                    + (h.bars / tagNames.length - barPadding + 7) / 2));
+           })
+           .style("font-family", "sans-serif")  // TODO why does changing color/size not
+           .style("font-size", "13px")          // seem to affect the visualization?
+           .style("fill", tagNameColor) // TODO put this in the CSS
+           .style("text-anchor", "left")
+           .attr("class", "labels");
     }
+
+    var buildWordCloud = function(params){
+        dataURL = params.dataURL || dataURL;
+        w.cloud = params.width || w.default;
+        h.cloud = params.height || h.default;
+        locationInDOM = params.locationInDOM || locationInDOM;
+        barPadding = params.barPadding || barPadding;
+        barColor = params.barColor || barColor;
+        barLabelColor = params.barLabelColor || barLabelColor;
+        tagNameColor = params.tagNameColor || tagNameColor;
+        clickHandler = params.clickHandler || clickHandler;
+        // key = params.keyFunction || key;
+
+        /* 'function' is a callback function to be run once /es/ has
+         * been fetched. */
+        d3.json(dataURL, function(
+            error, jsonList) {
+            if (error) return console.warn(error);
+
+            dataset = getTagCounts(jsonList); // pass the jsonList dict to separate its tags
+            console.log(dataset);
+            /* pass it a function as to what to do on click */
+            drawWordCloud(dataset, locationInDOM, clickHandler);
+        });
+
+    };
+
+    var drawWordCloud = function(dataset, locationInDOM, clickHandler) {
+
+        var max_tagName_len_pixels;
+        var left_margin;
+        var char_width = 6; // assume char width is 6 pixels
+                            // TODO better soln for this?
+
+        var min_text_size = 8;
+        var max_text_size = 32;
+
+        var tagNames = Object.getOwnPropertyNames(dataset);
+
+        /* don't let tagNames be more than 1/3 of svg width */
+        max_tagName_len_pixels = Math.min(
+            (d3.max(tagNames, function(tag){ return tag.length; }) * char_width),
+            w.cloud/3
+        );
+        max_tagName_len_chars = max_tagName_len_pixels / char_width;
+
+        left_margin = 0;
+        console.log(max_tagName_len_pixels);
+
+        var wordSizeScale = d3.scale.linear()
+                              .domain([0, d3.max(tagNames, function(d) {
+                                    return dataset[d];
+                                  })])
+                              .range([min_text_size, max_text_size]);
+
+        console.log("right side: " + (w.cloud - barPadding * 2));
+
+
+        /* sort tagNames according to tag count */
+        tagNames.sort(function(tag1, tag2){
+            return dataset[tag2] - dataset[tag1]; // compare function
+        });
+
+        console.log(d3.extent(tagNames, function(d){
+            return dataset[d];
+        }))
+
+
+        /* make list smaller if it's too many tags */
+        var maxNumTags = Math.floor(h.cloud/minBarPlusPaddingHeight);
+        if(tagNames.length > maxNumTags){
+            isEveryTagDisplayedInBarChart = false;
+            var tempTagNames = [];
+            for(var i=0; i<maxNumTags; i++){
+                tempTagNames[i] = tagNames[i];
+            }
+            tagNames = tempTagNames;
+        }
+
+        /* TODO use p and span */
+
+        var cloud = d3.select(locationInDOM).append("p");
+        // cloud.attr("width", w.cloud)  TODO put within div?
+        //    .attr("height", h.cloud);
+        var tagTexts = cloud.selectAll("span")
+                           .data(tagNames)
+                           .enter()
+                           .append("span")
+                           .attr("class", "cloudSpan")
+                           .text(function(d){
+                               /* modified from http://stackoverflow.com/a/7463674/4979097 */
+                               return (d +"("+ dataset[d] +")");
+                           })
+                           .style("font-family", "sans-serif")
+                           .style("font-size", function(tagName){
+                               return Math.floor(wordSizeScale(dataset[tagName]))+"px";})
+                           .style("color", "black");
+
+
+            // TODO how to account for diff. widths of text?
+
+        tagTexts.on("click", clickHandler);
+
+        // /* TODO remove this? or use it for adding values separately from above? */
+        // cloud.selectAll("text.values")
+        //    .data(tagNames)
+        //    .enter()
+        //    .append("text")
+        //    .text(function(d){
+        //        return ("");
+        //    })
+        //    .attr("x", function(d) {
+        //        return wordSizeScale(dataset[d]) * char_width; // TODO ? how to go between size of string in
+        //                             // chars vs in pixels? esp. when pixel size of chars can be variable?
+        //    })
+        //    .attr("y", function(d, i){
+        //        return ((i * (h.cloud / tagNames.length)
+        //             + (h.cloud / tagNames.length - barPadding + 7) / 2));
+        //    })
+        //    .attr("font-family", "sans-serif")  // TODO why does changing color/size not
+        //    .attr("font-size", "11px")          // seem to affect the visualization?
+        //    .attr("fill", barLabelColor)
+        //    .attr("text-anchor", "middle");
+
+
+    };
+
 
     /* expose to the user the variables/fcns we want exposed */
     exports.buildBarChart = buildBarChart; // no () bc we want the fcn, not
                                            // the result of calling the fcn.
     exports.isEveryTagDisplayedInBarChart = isEveryTagDisplayedInBarChart;
+
+    exports.buildWordCloud = buildWordCloud;
 
 })(app);
